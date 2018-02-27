fa45f8c25deaf1365459526425ca87665f8cadc9
Alexandru <Alexandru@DESKTOP-788I8A3>
2018-02-27 22:54:56 +0200

    First Homework - Producer_Consumer

---
+++ ProducerConsumer/ProducerConsumer/ProducerConsumer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+    public class ProducerConsumer
+    {
+        private Queue<int> _items;
+        private object _lockObject;
+
+        public ProducerConsumer()
+        {
+            _items = new Queue<int>(5);
+            _lockObject = new object();
+        }
+
+        public void Produce()
+        {
+            int count = 1;
+            int queueSize;
+
+            while (true)
+            {
+                lock (_lockObject)
+                {
+                    queueSize = _items.Count;
+                }     
+
+                if (queueSize < 5)
+                {
+                    Console.WriteLine("Produced: " + count);
+                    Thread.Sleep(100);
+
+                    lock (_lockObject)
+                    {
+                        _items.Enqueue(count);
+                    }
+
+                    count++;
+                }
+
+            }
+        }
+
+        public void Consume()
+        {
+            while (true)
+            {
+                Thread.Sleep(1500);
+                lock (_lockObject)
+                {
+                    if (_items.Count != 0)
+                    {
+
+                        Console.WriteLine("Consumed: " + _items.Dequeue());
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ ProducerConsumer/ProducerConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProducerConsumer pc = new ProducerConsumer();
+            Task p = Task.Factory.StartNew(() => pc.Produce());
+            Task c = Task.Factory.StartNew(() => pc.Consume());
+            Task.WaitAll(p, c);
+            Console.ReadKey();
+        }
+    }
+}
