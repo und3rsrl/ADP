bc8b8940567d4ecb8a5f1c28d5eac287750bfe9a
Alexandru <Alexandru@DESKTOP-788I8A3>
2018-03-11 11:14:23 +0200

    Homework 2 - Semaphore Producer Consumer

--- ProducerConsumer/ProducerConsumer/ProducerConsumer.cs
+++ ProducerConsumer/ProducerConsumer/ProducerConsumer.cs
@@ -6,59 +6,70 @@ namespace ProducerConsumer
 {
     public class ProducerConsumer
     {
+        Semaphore semaphoreFree = new Semaphore(5, 5);
+        Semaphore semaphoreFull = new Semaphore(0, 5);
         private Queue<int> _items;
         private object _lockObject;
 
         public ProducerConsumer()
         {
-            _items = new Queue<int>(5);
+            _items = new Queue<int>();
             _lockObject = new object();
         }
 
-        public void Produce()
+        public void Produce(string name)
         {
             int count = 1;
             int queueSize;
+            bool produced = false;
 
             while (true)
             {
+                semaphoreFree.WaitOne();
+                if (produced == false)
+                {
+                    Console.WriteLine(name + "/Produced: " + count);
+                    Thread.Sleep(50);
+                    produced = true;
+                }
+
                 lock (_lockObject)
                 {
                     queueSize = _items.Count;
-                }     
+                }
 
                 if (queueSize < 5)
                 {
-                    Console.WriteLine("Produced: " + count);
-                    Thread.Sleep(100);
-
                     lock (_lockObject)
                     {
                         _items.Enqueue(count);
+                        produced = false;
+                        Console.WriteLine(name + "/Pus in lista: " + count);
+                        count++;
+                        semaphoreFull.Release();
                     }
-
-                    count++;
                 }
-
             }
         }
 
-        public void Consume()
+        public void Consume(string name)
         {
+            int count = 0;
             while (true)
             {
-                Thread.Sleep(1500);
+                semaphoreFull.WaitOne();
+
                 lock (_lockObject)
                 {
                     if (_items.Count != 0)
                     {
-
-                        Console.WriteLine("Consumed: " + _items.Dequeue());
+                        count = _items.Dequeue();
+                        Console.WriteLine(name + "/Scos din lista: " + count);
                     }
                 }
-
+                semaphoreFree.Release(1);
+                Thread.Sleep(10000);   
             }
-
         }
     }
 }
