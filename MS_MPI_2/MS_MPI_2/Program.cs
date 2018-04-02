a920bb841957954f0da59aa6937af5c02d14de26
Alexandru <Alexandru@DESKTOP-788I8A3>
2018-04-02 22:27:20 +0300

    Homework 2 MPI

---
+++ MS_MPI_2/MS_MPI_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using MPI;
+using System;
+
+namespace MS_MPI_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Random random = new Random();
+
+            using (new MPI.Environment(ref args))
+            {
+                Intracommunicator comm = MPI.Communicator.world;
+
+                int master = -1;
+
+                int myNumber = random.Next(10);
+
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int i = 0; i < comm.Size; i++)
+                    {
+                        if (i == comm.Rank)
+                            continue;
+
+                        comm.Send(myNumber, i, 0);
+                        int receivedNumber = comm.Receive<int>(i, 0);
+
+                        if (receivedNumber > myNumber)
+                        {
+                            if (receivedNumber == myNumber)
+                            {
+                                if (comm.Rank > i)
+                                    master = comm.Rank;
+                                else
+                                {
+                                    master = i;
+                                }
+                            }
+                            else
+                            {
+                                master = i;
+                            }
+                        }
+                        else
+                        {
+                            master = comm.Rank;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Proces " + comm.Rank + ": " + master + "/ My number : " + myNumber);
+            }
+        }
+    }
+}
